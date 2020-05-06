7de82de618c7683d08283b107c2acd6cf0c6aa6e
Georgi <56259397+deepcodeg@users.noreply.github.com>
2020-05-06 16:34:33 +0200

    Create crit+warn.java

---
+++ crit+warn.java
@@ -0,0 +1,19 @@
+function replace1(str) {
+  return str.replace(/"/, '&quot');
+}
+function replace2(str) {
+  return str.replace(/&/, '&amp');
+}
+function replace3(str) {
+  return str.replace(/</, '&lt');
+}
+function replace1(str) {
+  return str.replace(/"/, '&quot');
+}
+function replace2(str) {
+  return str.replace(/&/, '&amp');
+}
+function replace3(str) {
+  return str.replace(/</, '&lt');
+}
+
